63b32db0ec4c1153ed0a431ad18e33af54b2bd61
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2017-10-24 15:55:57 +0300

    Json client-feature: add request serialization

--- ktor-client/ktor-client-core/src/io/ktor/client/features/HttpPlainText.kt
+++ ktor-client/ktor-client-core/src/io/ktor/client/features/HttpPlainText.kt
@@ -11,7 +11,7 @@ import java.io.*
 import java.nio.charset.*
 
 
-class HttpPlainText(private val defaultCharset: Charset) {
+class HttpPlainText(val defaultCharset: Charset) {
     suspend fun read(response: HttpResponseBuilder): String? {
         val payload = response.body as? HttpMessageBody ?: return null
         val charset = response.headers.charset() ?: defaultCharset
